900215cdfd97425cda34e87943e4569a626d504f
Regmi <sregmi1@ford.com>
2016-03-15 11:42:09 -0700

    Added Distress Activity
    - Added Activity for Distress

---
+++ Android/app/src/main/java/com/ford/emergencyconnect/DistressScreen.java
@@ -0,0 +1,17 @@
+package com.ford.emergencyconnect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sregmi1 on 3/15/16.
+ */
+public class DistressScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distress_screen);
+    }
+}
