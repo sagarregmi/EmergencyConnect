5735efe31f17bd2c85b57eda1bd0b05e59e908f6
Carpenter <OCARPEN4@ford.com>
2016-03-17 10:29:45 -0700

    Added GPS tracking to DistressScreen to send accurate GPS with DistressMessage

--- Android/app/src/main/java/com/ford/emergencyconnect/DistressScreen.java
+++ Android/app/src/main/java/com/ford/emergencyconnect/DistressScreen.java
@@ -1,16 +1,32 @@
 package com.ford.emergencyconnect;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+
+import java.util.Calendar;
 
 /**
  * Created by sregmi1 on 3/15/16.
  */
-public class DistressScreen extends Activity {
+public class DistressScreen extends Activity implements LocationListener {
+    double lat;
+    double lng;
+    LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +36,77 @@ public class DistressScreen extends Activity {
         Firebase.setAndroidContext(this);
         final Firebase ref = new Firebase("https://emergencyconnect.firebaseio.com/");
 
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Debug", "Permissions error");
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            Log.i("Debug", "Last known location null");
+        }
+
         Button sendDistress = (Button) findViewById(R.id.crashButton);
         sendDistress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DistressMessage message = new DistressMessage(37.4108709, -122.1462192, "Owen", 26, "None", "555-555-5555", 0);
+                DistressMessage message = new DistressMessage(lat, lng,
+                        "Owen", 26, "None", "555-555-5555", 0);
                 ref.child("distress").push().setValue(message);
             }
         });
+    }
+
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Debug", "Permissions Error");
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Debug", "Permissions Error");
+            return;
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 }
