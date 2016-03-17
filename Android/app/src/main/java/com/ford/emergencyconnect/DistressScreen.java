bdecfec12d7779582e3cf009f9afc10beafd6841
Regmi <sregmi1@ford.com>
2016-03-17 14:23:08 -0700

    Corrected Merge error

--- Android/app/src/main/java/com/ford/emergencyconnect/DistressScreen.java
+++ Android/app/src/main/java/com/ford/emergencyconnect/DistressScreen.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
->>>>>>> Updated Login Activity
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
