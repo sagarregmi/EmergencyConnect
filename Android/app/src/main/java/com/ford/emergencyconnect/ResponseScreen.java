f11e11091b84d2ad8db99ec603310b834380e14c
Carpenter <OCARPEN4@ford.com>
2016-03-17 12:39:54 -0700

    Added GPS tracking code to ResponseScreen

--- Android/app/src/main/java/com/ford/emergencyconnect/ResponseScreen.java
+++ Android/app/src/main/java/com/ford/emergencyconnect/ResponseScreen.java
@@ -1,8 +1,16 @@
 package com.ford.emergencyconnect;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,12 +24,35 @@ import com.firebase.client.FirebaseError;
 
 import java.util.Date;
 
-public class ResponseScreen extends AppCompatActivity {
-
+public class ResponseScreen extends AppCompatActivity implements LocationListener {
+    double lat;
+    double lng;
+    LocationManager locationManager;
+    private String provider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_response_screen);
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Debug", "Permissions error");
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            Log.i("Debug", "Last known location null");
+        }
 
         Firebase.setAndroidContext(this);
         final Firebase ref = new Firebase("https://emergencyconnect.firebaseio.com/");
@@ -43,7 +74,9 @@ public class ResponseScreen extends AppCompatActivity {
                             dataSnapshot.child("phoneNumber").getValue().toString(),
                             Integer.parseInt(dataSnapshot.child("numPassengers").getValue().toString()));
                     tv.setText(message.toString());
-                    //Determine if GPS location is close enought
+                    //Determine if GPS location is close enough
+                    Log.i("Debug", "DistressGPS " + message.lat + ", " + message.lng);
+                    Log.i("Debug", "ResponseGPS " + lat + ", " + lng);
                 }
             }
 
@@ -69,4 +102,43 @@ public class ResponseScreen extends AppCompatActivity {
         });
     }
 
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Debug", "Permissions Error");
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Debug", "Permissions Error");
+            return;
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
