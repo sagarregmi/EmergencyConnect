d712bf353f70f90d86d4879a52b77a04e13a4d7e
Sagar Regmi <sregmi1@ford.com>
2016-03-29 13:45:07 -0700

    Update to Distress Activity UI
    - Updated Layout and Color
    - Updated Action bar to add icon and menu

---
+++ Android/app/src/main/java/com/ford/emergencyconnect/NumberPicker.java
@@ -0,0 +1,43 @@
+package com.ford.emergencyconnect;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by sregmi1 on 3/29/16.
+ */
+public class NumberPicker extends android.widget.NumberPicker {
+
+    public NumberPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void addView(View child) {
+        super.addView(child);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, int index, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, params);
+        updateView(child);
+    }
+
+    private void updateView(View view) {
+        if(view instanceof EditText){
+            ((EditText) view).setTextSize(20);
+            ((EditText) view).setTextColor(getResources().getColor(R.color.LightRed2));
+        }
+    }
+
+}
